7bf4bc5b1351f59e8af1d62336c7868d3f4b57a2
03arpit04 <03arpit04@gmail.com>
2026-06-18 22:42:21 +0530

    Replace site favicon logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -87,8 +87,8 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", type: "image/jpeg", href: "/favicon.jfif" },
-      { rel: "shortcut icon", type: "image/jpeg", href: "/favicon.jfif" },
+      { rel: "icon", type: "image/png", href: "/favicon.png" },
+      { rel: "shortcut icon", type: "image/png", href: "/favicon.png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
